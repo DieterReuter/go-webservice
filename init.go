d9ab6e7f5257fd596ae91ad0d83b9bec8b0c9dda
Dieter Reuter <dieterreuter@me.com>
2014-11-08 19:59:05 +0100

    Change api port to api server port

--- init.go
+++ init.go
@@ -5,7 +5,7 @@ import (
 )
 
 func init() {
-	flag.IntVar(&API_PORT, "port", 10777, "api port")
+	flag.StringVar(&API_SERVERPORT, "server", "127.0.0.1:10777", "api server port")
 	flag.StringVar(&CONFIG_FILE, "conf", "./service.conf", "config file")
 	flag.StringVar(&DATASTORE_DIR, "datastore", "./datastore/", "datastore dir")
 	flag.Parse()
