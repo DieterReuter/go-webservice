6ba3314ec4c5b0ea12cd59faf6f2bd5c4a1af143
Dieter Reuter <dieterreuter@me.com>
2014-11-08 17:40:48 +0100

    Use directories with trailing /

--- init.go
+++ init.go
@@ -7,6 +7,6 @@ import (
 func init() {
 	flag.IntVar(&API_PORT, "port", 10777, "api port")
 	flag.StringVar(&CONFIG_FILE, "conf", "./service.conf", "config file")
-	flag.StringVar(&DATASTORE_DIR, "datastore", "./datastore", "datastore dir")
+	flag.StringVar(&DATASTORE_DIR, "datastore", "./datastore/", "datastore dir")
 	flag.Parse()
 }
