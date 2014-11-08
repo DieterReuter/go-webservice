1f8beca86c211800186beacf72cd2acc058651d6
Dieter Reuter <dieterreuter@me.com>
2014-11-08 20:19:46 +0100

    Set Content-Type and Api-Service

--- api.go
+++ api.go
@@ -5,19 +5,27 @@ import (
 )
 
 func api_versions(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Api-Service", "-")
 	w.Write([]byte("/api/v0,/api/v1"))
 }
 
 func api_v0_version(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Api-Service", "v0")
 	w.Write([]byte("v0.1234"))
 }
 
 func api_v1_version(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Api-Service", "v1")
 	w.Write([]byte("v1.2345"))
 }
 
 func api_user_profile(w http.ResponseWriter, r *http.Request) {
 	//params := mux.Vars(r)
 	//name := params["name"]
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Api-Service", "-")
 	w.Write([]byte("Hello World"))
 }
